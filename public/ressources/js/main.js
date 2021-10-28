4e70d34b7701bc63c895a931104f07465f0df1de
VoidSplit <46133265+VoidSplit@users.noreply.github.com>
2021-10-28 21:53:06 +0200

    Add comments #1
    Adding a lot of comments
    #1

---
+++ public/ressources/js/main.js
@@ -0,0 +1,141 @@
+/**
+ * define all sections & these actions (buttons)
+ */
+let homeSection = document.getElementById('home')
+let joinSection = document.getElementById('join')
+let createSection = document.getElementById('create')
+let waitingSection = document.getElementById('waiting');
+
+function joinParty() {
+  homeSection.classList.toggle('disabled')
+  joinSection.classList.toggle('disabled')
+}
+function createParty() {
+  homeSection.classList.toggle('disabled')
+  createSection.classList.toggle('disabled')
+}
+function cancel() {
+  homeSection.classList.remove('disabled')
+  joinSection.classList.add('disabled')
+  createSection.classList.add('disabled')
+}
+
+/**
+ * get all informations from the create and join section's form
+ */
+
+const usernameInput = document.getElementById('username');
+const joinUsernameInput = document.getElementById('joinUsername');
+const roomNameInput = document.getElementById('roomName');
+const maxPlayersInput = document.getElementById('maxPlayer');
+const privateCheck = document.getElementById('privacity');
+const roomListDOM = document.getElementById('roomList');
+const linkToShare = document.getElementById('link-to-share');
+
+/**
+ * define the player object template
+ */
+
+const player = {
+  host: false,
+  roomId: null,
+  roomName: "",
+  roomMaxPlayers: null,
+  deck: {},
+  pass: false,
+  username: "",
+  socketId: "",
+  private: false,
+  turn: false,
+  win: false
+};
+/**
+ * get the rooms and display them
+ */
+socket.emit('get rooms');
+socket.on('list rooms', (rooms) => {
+  let html = "";
+
+  if(rooms.length > 0) {
+    rooms.forEach(room => {
+      if(room.players.length !== 2) {
+        html += `<div data-room="${room.id}" class="item">
+                  <p class="name">${room.players[0].roomName}</p>
+                  <p class="count">1/${room.players[0].roomMaxPlayers}</p>
+                  <button class="roomButton">Rejoindre la salle</button>
+                </div>`;
+      }
+    });
+  }
+  /**
+   * adding event listener to the buttons on room list items
+   */
+  if(html !== "") {
+    roomListDOM.innerHTML = html;
+    for(const element of document.getElementsByClassName('roomButton')) {
+      element.addEventListener('click', joinRoom, false)
+    }
+  }
+})
+/**
+ * get the create section's form and fill the player template with theses informations
+ */
+$("#formCreate").on('submit', function (e) {
+  e.preventDefault();
+  player.username = usernameInput.value;
+  player.host = true;
+  player.turn = true;
+  player.socketId = socket.id;
+  player.roomName = roomNameInput.value;
+  player.roomMaxPlayers = maxPlayersInput.value;
+  player.private = privacity.checked;
+  waitingSection.classList.remove('disabled')
+  createSection.classList.add('disabled')
+
+  // emit the playerData to the server
+  socket.emit('playerData', player);
+})
+
+/**
+ * when the game start we call the function startGame()
+ */
+socket.on("start game", (players) => {
+  startGame(players);
+});
+/**
+ * when the owner join a room we display the link to share the room with other people
+ */
+socket.on('join room', (roomId) => {
+  player.roomId = roomId;
+  linkToShare.innerHTML = `<a href="${window.location.href}?room=${player.roomId}" target="_blank">${window.location.href}?room=${player.roomId}</a>`;
+  console.log("Utilisateur connecté a la salle")
+});
+/*
+TODO start the game 
+TODO give all players a deck
+TODO ...
+*/
+function startGame(players) {
+  console.log("start")
+}
+/**
+ * when someone is joining the room with the buttons this function is called
+ */
+const joinRoom = function() {
+  // if player has choose a name
+  if(joinUsernameInput.value !== "") {
+    player.username = joinUsernameInput.value;
+    player.socketId = socket.id;
+    player.roomId = this.dataset.room;
+
+    socket.emit("playerData", player);
+  }
+  // if player didn't choose a name
+  else {
+    player.username = "Anonymous";
+    player.socketId = socket.id;
+    player.roomId = this.dataset.room;
+
+    socket.emit("playerData", player);
+  }
+}
--- server.js
+++ server.js
@@ -5,50 +5,82 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-let rooms = [];
-
+let rooms = []; // define the list of the rooms actually not empty
+/**
+ * define all the Routes (jquery & public directory)
+ */
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
-
 app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+/**
+ * when user is looking the page he's connected
+ */
 
 io.on('connection', (socket) => {
   console.log(`[user connexion] ${socket.id}`);
 
+  /**
+   * playerData is a emiter wich contain all the player informations (host, roomId, roomName, roomMaxPlayers, deck, etc..)
+   * it is emit when all the player's information is collected
+   */
   socket.on('playerData', (player) => {
     console.log(`[playerData] ${player.username}`)
-    let room = null;
 
+    let room = null; // clear the temp variable
+
+    /**
+     * if the player does not have a room, we create one for him
+     */
     if(!player.roomId) {
       room = createRoom(player);
       console.log(`[create room] ${room.id} ${player.username}`)
     } else {
+      /**
+       * else, we push the player into the room
+       */
       room = rooms.find(r => r.id == player.roomId);
 
-      if(room === undefined) {
+      if(room === undefined) { // if the room is undefined we cancel the action
         return;
       }
-      room.players.push(player);
+      room.players.push(player); // push the player on the room players list
     }
-    socket.join(room.id);
+    /**
+     * after the player has joined the list he is made to join the room
+     */
+    socket.join(room.id); 
+
     io.to(socket.id).emit('join room', room.id)
 
+    /**
+     * if the room has 2 players, the game start
+     */
+    // TODO Create an interface with a start game button
     if(room.players.length === 2) {
       io.to(room.id).emit('start game', room.players)
     }
   });
 
+  /**
+   * this collector is use to get the rooms and send to the client to display them on the join section
+   */
   socket.on('get rooms', () => {
     io.to(socket.id).emit('list rooms', rooms)
   })
-
+  /**
+   * when disconnected, the player's room will be deleted
+   */
   socket.on('disconnect', () => {
     console.log(`[user disconnect] ${socket.id}`);
-    let room = null;
+    let room = null; // clear the temp variable
 
+    /**
+     * we filter all the rooms availables
+     */
     rooms.forEach(r => {
       r.players.forEach(p => {
         if(p.socketId === socket.id && p.host) {
@@ -60,10 +92,16 @@ io.on('connection', (socket) => {
   });
 });
 
+/**
+ * we listen the port 3000 to display the website on this port
+ */
 server.listen(3000, () => {
   console.log('listening on port 3000');
 });
 
+/**
+ * this function create the room 
+ */
 function createRoom(player) {
   const room = {id: roomId(), players: []};
 
@@ -73,6 +111,10 @@ function createRoom(player) {
 
   return room;
 }
+
+/**
+ * this function create the room ID
+ */
 function roomId() {
   return Math.random().toString(36).substr(2, 9);
 }
